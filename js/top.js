1aa52fe02aa54063153b04870cbf25cfa1978ef9
chi_xu <chi_xu@umail.ucsb.edu>
2020-04-20 23:45:25 -0700

    update 3

---
+++ js/top.js
@@ -0,0 +1,26 @@
+//reference: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+var mybutton = document.getElementById("up_btn");
+
+var body = document.body,
+    html = document.documentElement;
+
+var height = Math.max( body.scrollHeight, body.offsetHeight,
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop >= 0.1*height || document.documentElement.scrollTop >= 0.1*height) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
