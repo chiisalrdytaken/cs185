7ac1b5c69d5e2ba4d70b9044c9be094abdd4869d
chi_xu <chi_xu@umail.ucsb.edu>
2020-05-01 00:59:27 -0700

    Updates

---
+++ precache-manifest.28332e7c9be19e7eab4f8ccc08b0ad07.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8893827385c653a98323583ddb2e216",
+    "url": "/cs185_2/index.html"
+  },
+  {
+    "revision": "a4f710a8097c3f73592f",
+    "url": "/cs185_2/static/css/main.c89eaefe.chunk.css"
+  },
+  {
+    "revision": "c02e3453c81bc7362c66",
+    "url": "/cs185_2/static/js/2.927fa209.chunk.js"
+  },
+  {
+    "revision": "4e6d57ea575a16b77026958ba0efff35",
+    "url": "/cs185_2/static/js/2.927fa209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f710a8097c3f73592f",
+    "url": "/cs185_2/static/js/main.99fc5eb6.chunk.js"
+  },
+  {
+    "revision": "c446781c964592f8f1bc",
+    "url": "/cs185_2/static/js/runtime-main.9054fbfd.js"
+  },
+  {
+    "revision": "7d34b6b66a27718bc2c2c6199064737f",
+    "url": "/cs185_2/static/media/b3.7d34b6b6.jpg"
+  },
+  {
+    "revision": "9342c259096f79708007639b88ba45f5",
+    "url": "/cs185_2/static/media/p01.9342c259.jpg"
+  },
+  {
+    "revision": "5ec067b393d4452eb568527975cd17e2",
+    "url": "/cs185_2/static/media/p02.5ec067b3.jpg"
+  },
+  {
+    "revision": "60fce56c1255161160af48379d5d85eb",
+    "url": "/cs185_2/static/media/p03.60fce56c.jpg"
+  },
+  {
+    "revision": "95230ecf792c5a741e166cac6f52023a",
+    "url": "/cs185_2/static/media/p04.95230ecf.jpg"
+  },
+  {
+    "revision": "7c413de1f7f5c3c809d807e5989dd2fd",
+    "url": "/cs185_2/static/media/p05.7c413de1.jpg"
+  },
+  {
+    "revision": "182a3c655d31fe27895c5eee1928bba2",
+    "url": "/cs185_2/static/media/p06.182a3c65.jpg"
+  },
+  {
+    "revision": "8b5efe408629e241af3612f3c01568e3",
+    "url": "/cs185_2/static/media/p07.8b5efe40.jpg"
+  },
+  {
+    "revision": "6593f240053fd9abe3ee48dc01f381f1",
+    "url": "/cs185_2/static/media/p08.6593f240.jpg"
+  },
+  {
+    "revision": "4bd836d1d88a30002a74509eb7cc6979",
+    "url": "/cs185_2/static/media/pika1.4bd836d1.jpg"
+  }
+]);
